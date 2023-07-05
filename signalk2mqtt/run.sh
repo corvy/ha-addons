d9aac2a22d36171e33646e7dc7832130f8a62b9f
Stian B. Barmen <stian@barmen.nu>
2023-07-05 07:46:39 +0200

    First add of new plugin signalk2mqtt

--- signalk2mqtt/run.sh
+++ signalk2mqtt/run.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/with-contenv bashio
 
+export IS_IN_DOCKER=true
+export DEFAULTENABLEDPLUGINS=signalk-mqtt-client
+#export ADMINUSER=admin:test123
+# export PORT=3001
+
+
+# Start reverse proxy
+/usr/sbin/nginx
+
+# Start Signal K Server
+/usr/local/bin/signalk-server --sample-nmea0183-data
+
