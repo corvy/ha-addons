037dfdcb83d73ab59aa565a3ace37e45eeecc983
Stian B. Barmen <stian@barmen.nu>
2024-03-31 18:28:06 +0200

    Debugging

--- gpsd2mqtt/gpsd2mqtt.py
+++ gpsd2mqtt/gpsd2mqtt.py
@@ -106,13 +106,13 @@ json_config = '''{{
     "payload_home": "home",
     "payload_not_home": "not_home",
     "payload_reset": "check_zone",
-    "json_attributes_topic": "{mqtt_attr}",
-    "device": {
-        "name": "GPSD Service",                         # This must match for all entities (if more are needed) 
-        "identifiers": "gpsd-{unique_identifier}",      # This must match for all entities (if more are needed) 
-        "configuration_url": "https://github.com/corvy/ha-addons/tree/main/gpsd2mqtt",
-        "model": "gpsd2MQTT",
-        "manufacturer": "GPSD"
+    "json_attributes_topic": "{mqtt_attr}" #,
+ #   "device": {
+ #       "name": "GPSD Service",                         # This must match for all entities (if more are needed) 
+ #       "identifiers": "gpsd-{unique_identifier}",      # This must match for all entities (if more are needed) 
+ #       "configuration_url": "https://github.com/corvy/ha-addons/tree/main/gpsd2mqtt",
+ #       "model": "gpsd2MQTT",
+ #       "manufacturer": "GPSD"
     }
 }}'''.format(mqtt_state=mqtt_state, mqtt_attr=mqtt_attr)
 
