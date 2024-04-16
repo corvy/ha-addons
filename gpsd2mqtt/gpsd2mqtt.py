8fe46ecd4d74ce3f4b9af8c8127545127ac72b82
Stian B. Barmen <stian@barmen.nu>
2024-04-16 11:15:47 +0200

    Improve wording of logg

--- gpsd2mqtt/gpsd2mqtt.py
+++ gpsd2mqtt/gpsd2mqtt.py
@@ -313,7 +313,7 @@ while True:
                 time_elapsed = (datetime.datetime.now() - last_summary_time).total_seconds() // 60
 
                 # Print the summary message
-                summary_message = f"Published {published_updates} location updates to the device_tracker in the last {time_elapsed} minutes. Achieved {accuracy}, in coverage of {n_satellites} of required {min_n_satellites} GPS satellites."
+                summary_message = f"Published {published_updates} location updates to the device_tracker in the last {time_elapsed} minutes. Achieved {accuracy}, in coverage of {n_satellites} of required {min_n_satellites} GPS satellites (at the time of log - could have had better fix in last logging periode)."
                 logger.info(summary_message)
 
                 # Reset the counters
